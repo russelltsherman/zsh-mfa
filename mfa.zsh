dbaf59173f2335fafa1974227e4b80f3ad394347
Russell <russell.t.sherman@gmail.com>
2021-01-02 16:13:48 -0600

    convert mfa to function

--- mfa.zsh
+++ mfa.zsh
@@ -1,2 +1,36 @@
-0=${(%):-%N}
-export PATH=${0:A:h}/bin:$PATH
+
+if chk::command "oathtool"
+then
+
+    mfa() {
+        profile="${1}"
+        file="${XDG_CONFIG_HOME}/mfa/${profile}.mfa"
+
+        if [ -f "$file" ]
+        then
+            code="$(oathtool --base32 --totp "$(cat "$file")")"
+            echo "$code" # output the code to screen
+            echo "$code" | pbcopy # push code into system clipboard
+        elif [ -z "$profile" ]
+        then
+            echo "No MFA profile defined" >&2
+        else
+            echo "No MFA profile for $profile" >&2
+        fi
+    }
+
+else
+  echo "oathtool not found. execute 'mfa::install' to install it."
+
+  mfa::install() {
+    if chk::osx
+    then
+        brew install oath-toolkit
+    fi
+
+    if chk::linux
+    then
+        apt-get install oath-toolkit
+    fi
+  }
+fi
